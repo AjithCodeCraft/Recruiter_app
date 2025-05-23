91aa744b825752ee1c27731295440718caa944eb
alameen27 <alameena068@gmail.com>
2025-05-24 00:35:39 +0530

    changed acces token t 1 day

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     GMAIL_USER: str
     GMAIL_PASSWORD: str  
     GMAIL_SENDER: str = "alameena068@gmail.com"
