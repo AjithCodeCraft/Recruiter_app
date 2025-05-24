144055e8f0d10d9dbded00b65653ed23b42ad342
Your Name <ajithsa900@gmail.com>
2025-05-24 12:56:07 +0530

    added auth lock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// src/App.tsx
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/auth/Login";
 import RegisterPage from "./pages/auth/Register";
 import Home from './pages/Dashboard/Home'; 
 import AddUser from './pages/Dashboard/Add-User';
 import UsersManagement from './pages/Dashboard/Users';
-// import DashboardLayout from './layouts/DashboardLayout'; 
 import DashboardLayout from './components/DashboardLayout';
 import EditUser from './pages/Dashboard/Edit-User';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
+    <AuthProvider>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         
-        <Route element={<DashboardLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/adduser" element={<AddUser />} />
-           <Route path="/users" element={<UsersManagement />} />
-           <Route path="/users/edit/:id" element={<EditUser />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<DashboardLayout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/adduser" element={<AddUser />} />
+            <Route path="/users" element={<UsersManagement />} />
+            <Route path="/users/edit/:id" element={<EditUser />} />
+          </Route>
         </Route>
       </Routes>
-    </Router>
+    </AuthProvider>
   );
 }
 
